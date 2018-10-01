bf21b730d86636a5e1b0f9538310ef0dfc3dbe9e
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2018-10-01 18:36:22 +0200

    Excercises

--- HolaMundo/HolaMundo/Program.cs
+++ HolaMundo/HolaMundo/Program.cs
@@ -10,9 +10,224 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo");
+            //    To declare a variable
+
+            //   String hola = "Hola Mundo";
+            //   Console.WriteLine(hola);
+
+            //    create Hola Natalia!
+
+            //   String name = "Hola Natalia";
+            //   Console.WriteLine(name);
+
+            //    sum of two numbers
+
+            //   int num1 = 4, num2 = 10;
+
+            //   Console.WriteLine(num1 + num2);
+
+            //    Divison of two numbers
+
+            //   Console.WriteLine(num2/num1);
+
+            //    -5 + 8*6
+
+            //   Console.WriteLine(-5 + 8 * 6);
+
+            //    (55+9) % 9
+
+            //   Console.WriteLine((55 + 9) % 9);
+
+            //   20 + -3*5/8
+
+            //   Console.WriteLine(20 + -3 * 5 / 8);
+
+            //    5 + 15/3*2 - 8%3
+
+            //   Console.WriteLine(5 + 15 / 3 * 2 - 8 % 3);
+
+            //    Escribe un programa que pida dos números al usuario y muestre el resultado de multiplicar los dos
+
+            //   Console.WriteLine("Introduce one number");
+
+            //   int num3;
+
+            //   num3 = Convert.ToInt32(Console.ReadLine());
+
+            //    convert a string into number
+            //   Console.WriteLine("Introduce one more number");
+
+            //   int num4;
+
+            //    convert a string into number
+            //   num4 = Convert.ToInt32(Console.ReadLine());
+
+            //   Console.WriteLine("Multiplication: " + num3 + " y " + num4 + " is " + (num3*num4));
+
+            //    Escribe un programa que pida un número al usuario y muestre la tabla de multiplicar hasta 10 de ese número.
+
+            //   Console.WriteLine("Type a number");
+            //   int num5;
+
+            //   num5 = Convert.ToInt32(Console.ReadLine());
+
+            //   int a = 0;
+
+            //   while (a < 11)
+            //   {
+            //       Console.WriteLine(num5 * a);
+            //       a++;
+            //   }
+
+            //   /* Escribe un programa que calcule el resultado de la siguiente operación:
+            //   *((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+            //   */
+
+            //   Console.WriteLine((((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))));
+
+            //    Escribe un programa que calcule el resultado de la siguiente operación:
+            //   Console.WriteLine((4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11))));
+
+            //   /*
+            //    * Escribe un programa que pida al usuario el radio de un círculo y que calcule 
+            //    * y muestre el área y el perímetro
+            //    * (el área de un círculo es PI por radio cuadrado 
+            //    * y el perímetro es 2 por radio por PI). PI es = 3.141593
+            //    */
+
+            //   Console.WriteLine("Radius");
+
+            //   double num6;
+
+            //   num6 = Convert.ToInt32(Console.ReadLine());
+
+            //   const double PI = 3.141593;
+
+
+            //   Console.WriteLine("Area of the circle is " + (PI * (Math.Pow(num6, 2)) + " and perimeter " + (2 * num6 * PI)));
+
+            //   Escribe un programa que pida tres números al usuario y que calcule y muestre su media
+            //Type a number
+            //   double num7;
+            //   Console.WriteLine("Type a number");
+            //   num7 = Convert.ToInt32(Console.ReadLine());
+
+            //    Type one more number
+            //   double num8;
+            //   Console.WriteLine("Type a number");
+
+            //   num8 = Convert.ToInt32(Console.ReadLine());
+
+            //    Type the last number
+            //   double num9;
+            //   Console.WriteLine("Type a number");
+
+            //   num9 = Convert.ToInt32(Console.ReadLine());
+
+            //   Console.WriteLine((num7 + num8 + num9) / 3);
+
+            //   /* Escribe un programa que pida al usuario la anchura y altura de un rectángulo y que calcule
+            //    * y muestre su área y su perímetro (el área es la altura por la anchura y
+            //    * el perímetro es 2 por la suma de la altura y la anchura).
+            //    */
+
+            //   Console.WriteLine("Type the height of your rectangle");
+
+            //   double height;
+
+            //   height = Convert.ToInt32(Console.ReadLine());
+
+            //   Console.WriteLine("Type the width of your rectangle");
+
+            //   double width;
+
+            //   width = Convert.ToInt32(Console.ReadLine());
+
+            //   Console.WriteLine("The area of the rectangle is " + (height * width) + " and the perimeter is " + (2*(height + width)));
+
+            /* Escribe un programa que pida dos variables, y después intercambie sus valores 
+             *(si por ejemplo ponemos 8 en la variable
+            *a y 5 en la variable b, que al final del programa la variable a tenga el valor 5 y
+            * la variable b el valor 8).
+            */
+
+            //Console.WriteLine("Type a number");
+            //int num10;
+
+            //num10 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Type one more");
+
+            //int num11;
+
+            //num11 = Convert.ToInt32(Console.ReadLine());
+
+            //// create another var to be able to make a change
+
+            //int a; 
+
+            //a = num10;
+
+            //num10 = num11;
+
+            //num11 = a;
+
+            //Console.WriteLine("num10 is " + num10 + " and num11 is " + num11);
+
+            /* Escribe un programa que pida un número 
+            *de 3 dígitos y que muestre en pantalla el resultado de sumarlos.
+            */
+
+            Console.WriteLine("Type a three-digit number");
+
+            int a;
+
+            a = Convert.ToInt32(Console.ReadLine());
+
+            int b = a / 100;
+            int c = (a % 100) / 10;
+            int d = a % 100 - c*10;
+
+            Console.WriteLine("The sum is " + (b+c+d));
+
+
+            /* 
+             * Escribe un programa que pida al usuario un dígito (n) y calcule y muestre el valor de n+nn+nnn. 
+             * Por ejemplo si el usuario introduce 5 el programa calcularía 5 + 55 + 555
+             */
+
+            Console.WriteLine("Type a one-digit number");
+
+            int numero;
+
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("It's " + (numero + Convert.ToInt32(Convert.ToString(numero) + Convert.ToString(numero)) + Convert.ToInt32((Convert.ToString(numero) + Convert.ToString(numero) + Convert.ToString(numero)))));
+
+            // Another way: numero + numero*11+ numero*111
+
+            // Escribe un programa que pida al usuario un número y convierta el string a un int y lo muestre en pantalla.
+
+            Console.WriteLine("Type a number");
+
+            int num14;
+
+            num14 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num14);
+
+            // Escribe un programa que calcule el resto de dividir dos números sin utilizar el operador de división de resto (%)
+
+
+
+
+
+
+
 
             Console.ReadLine();
-        }
-    }
+
+
+}
+}
 }
